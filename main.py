2d9af91425e27ddefa4f584a4f2478b1858d241c
tofikhidayatxyz <topikhidayat711@gmail.com>
2021-12-10 18:57:54 +0700

    desc

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-class Car :
+class Kipas:
     brand = None
-    lamp = False
-    _gear = 1
-    _capacity = 1500
-    _speed = 0
+    rotasi = False
+    _gear = 3
+    _capacity = 103
 
-    def __init__(self, brand, capacity) :
+    def __init__(self, brand, capacity):
         self.brand = brand
         self._capacity = capacity
-
-    def setSpeed(self, speed):
-        self._speed = speed
-
-    def _getSPeed(self):
-        return  f"Speed: {self._speed} KM/H"
 
     def setGear(self, gear):
         self._gear = gear
@@ -22,67 +15,33 @@ class Car :
         return f"Gear : {self._gear}"
 
     def _getCapacity(self):
-        return f"Kapasistas {self._capacity}"
+        return f"Kapasistas {self._capacity} Watt"
 
-    def turnOnLamp(self):
-        self.lamp = True
+    def turnOnrotasi(self):
+        self.rotasi = True
 
-    def turnOffLamp(self):
-        self.lamp = False
+    def turnOffrotasi(self):
+        self.rotasi = False
 
-    def _getLamp(self):
-        return F"Lampu {'Nyala' if self.lamp else 'Mati'}"
+    def _getrotasi(self):
+        return F"rotasi {'nyala ' if self.rotasi else 'Mati'}"
 
     def detail(self):
         print("------------------------")
         print(f"Brand : {self.brand}")
         print(self._getGear())
-        print(self._getSPeed())
         print(self._getCapacity())
-        print(self._getLamp())
+        print(self._getrotasi())
         print("------------------------")
 
-
-# Subclass
-class Lamborgini(Car):
-    def __init__(self, type):
-        if(type == 'Aventador') :
-            capacity = 3000
-        elif (type == 'Huracan'):
-            capacity = 5000
-        else :
-            capacity = 2000
-
-        super(Lamborgini, self).__init__(F"Lamborgini {type}", capacity)
-
-    def setSpeed(self, speed):
-        self._speed = speed * self._gear
-    def setGear(self, gear):
-        self._gear = "Mundur" if gear < 0 else gear
+    # Descructor
+    def __del__(self):
+        print('Class Destroyed byebye :)')
 
 
 
-## Super class
-car = Car("Avanza", 1400)
-car.setGear(4)
-car.setSpeed(100)
-car.turnOnLamp()
-car.detail()
-car.turnOffLamp()
-car.detail()
+fan = Kipas("Miyako", 1200)
 
-## Subclass
+fan.detail();
 
-lambo = Lamborgini('Aventador')
-lambo.setGear(3)
-lambo.setSpeed(300)
-lambo.turnOnLamp()
-lambo.detail()
-lambo.setGear(-1)
-lambo.turnOffLamp()
-lambo.detail()
-
-
-
-
-
+del fan
